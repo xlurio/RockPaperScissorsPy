a6dd9863d7d670fd814d870c1354fd04c929908e
Lucas Calegario <74337880+xlurio@users.noreply.github.com>
2022-06-16 03:20:33 -0300

    Update core.py

--- core.py
+++ core.py
@@ -2,22 +2,6 @@ from src.choice import Choice, PlayerVsComChoicesFactory
 from src.round import Round, Output
 
 
-def get_user_choice():
-    user_choice = Choice('placeholder')
-    while not user_choice.is_valid():
-        entered_choice = input(Output.get_user_choice_header())
-        user_choice.set_choice(entered_choice)
-    return user_choice
-
-
-def get_com_choice():
-    valid_choices = Choices.VALID_CHOICES
-    number_of_valid_choices = len(valid_choices)
-    com_entry_index = randrange(number_of_valid_choices)
-    com_entry = valid_choices[com_entry_index]
-    return Choice(com_entry)
-
-
 def print_choices(choice1, choice2):
     round_choices = Output.get_round_choices(choice1, choice2)
     print(round_choices)
